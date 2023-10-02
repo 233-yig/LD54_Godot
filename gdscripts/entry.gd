268e40b02bef58b6b6413e6b927e83a3a34fe520
233-yig <425953692@qq.com>
2023-10-03 04:05:20 +0800

    update

--- gdscripts/entry.gd
+++ gdscripts/entry.gd
@@ -3,12 +3,14 @@ extends Control
 var current :int = 0
 
 var scene: PackedScene = preload("res://scenes/main_game.tscn");
+func _process(delta):
+	self.custom_minimum_size = get_viewport_rect().size
 func Resume():
 	$ui.visible = true
 
 func NextLevel():
 	var game:Node = scene.instantiate()
-	add_child(game)
+	self.add_child(game)
 	game.Start(levels[current])
 	game.tree_exited.connect(self.Resume)
 	$ui.visible = false
--- gdscripts/main_game.gd
+++ gdscripts/main_game.gd
@@ -1,13 +1,11 @@
 extends Control
 
-
-
 @export var tile: Texture2D
 var mapSize: Vector2i
 func Start(param):
 	mapSize = param.mapSize;
-	self.custom_minimum_size = Vector2(mapSize.x - 1, mapSize.y - 1) * tile.get_size()
-	$GameBoard.load_data(
+	$MainGame.custom_minimum_size = Vector2(mapSize.x, mapSize.y) * tile.get_size()
+	$MainGame/GameBoard.load_data(
 		param.mapSize,
 		param.mineCount,
 		param.maxFlips,
@@ -18,16 +16,17 @@ func _input(event: InputEvent):
 	if !(event is InputEventMouse):
 		return
 	#var mousePos: Vector2 = self.get_local_mouse_position();
-	var pos: Vector2i = self.get_local_mouse_position() / tile.get_size() + Vector2(0.5,0.5)
+	var pos: Vector2i = $MainGame.get_local_mouse_position() / tile.get_size() * $MainGame.scale
+	print(pos)
 	if pos.x < 0 || pos.x >= mapSize.x || pos.y < 0 || pos.y >= mapSize.y:
 		return
-	$Cursor.position = (Vector2(pos) - Vector2(0.5,0.5)) * tile.get_size();
+	$MainGame/Cursor.position = $MainGame.get_local_mouse_position() - tile.get_size() * $MainGame/Cursor.scale;
 	if !(event is InputEventMouseButton):
 		return
 	var ret;
 	match(event.button_index):
 		MOUSE_BUTTON_LEFT:
-			ret = $GameBoard.flip(pos);
+			ret = $MainGame/GameBoard.flip(pos);
 		MOUSE_BUTTON_RIGHT:
-			ret = $GameBoard.flag(pos);
+			ret = $MainGame/GameBoard.flag(pos);
 	pass
