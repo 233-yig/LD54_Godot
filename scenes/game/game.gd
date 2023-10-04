72479f88669befe8172473c857ff45426c59eefb
233-yig <425953692@qq.com>
2023-10-04 18:50:19 +0800

    update

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,8 +1,6 @@
 extends Control
 @export var levels: Array[LevelData]
 
-var cur_mines: int
-var cur_flips: int
 var cur_level: LevelData
 func InitializeLevel(idx: int):
 	if idx < 0 || idx >= levels.size():
@@ -11,25 +9,19 @@ func InitializeLevel(idx: int):
 		return
 	cur_level = levels[idx]
 	
-	$GameUI.SetData(cur_mines, cur_level.mineCount, cur_flips, cur_level.maxFlips)
+	$GameUI.SetData($MainGame/GameBoard.flag_count(), cur_level.mineCount, $MainGame/GameBoard.flip_count(), cur_level.maxFlips)
 	$GameUI.SetDialog(cur_level.start_dialog)
 	$MainGame.custom_minimum_size = Vector2(cur_level.mapSize) * $MainGame/Background.texture.get_size() / 3
 	$MainGame/GameBoard.load_data(cur_level.mapSize, cur_level.mineCount, cur_level.maxFlips, cur_level.mapStr)
 func flip(pos: Vector2i):
 	var ret = $MainGame/GameBoard.flip(pos)
-	if ret == $MainGame/GameBoard.OpResult_Success && cur_flips < cur_level.maxFlips:
-		cur_flips += 1
-		$GameUI.SetData(cur_mines, cur_level.mineCount, cur_flips, cur_level.maxFlips)
-	elif ret == $MainGame/GameBoard.OpResult_Lose:
+	$GameUI.SetData($MainGame/GameBoard.flag_count(), cur_level.mineCount, $MainGame/GameBoard.flip_count(), cur_level.maxFlips)
+	if ret == $MainGame/GameBoard.OpResult_Lose:
 		$GameUI.SetDialog(cur_level.lose_dialog)
 func flag(pos: Vector2i):
 	var ret = $MainGame/GameBoard.flag(pos);
-	if ret == $MainGame/GameBoard.OpResult_Success:
-		cur_mines += 1
-		$GameUI.SetData(cur_mines, cur_level.mineCount, cur_flips, cur_level.maxFlips)
-	elif ret == $MainGame/GameBoard.OpResult_Win:
-		cur_mines += 1
-		$GameUI.SetData(cur_mines, cur_level.mineCount, cur_flips, cur_level.maxFlips)
+	$GameUI.SetData($MainGame/GameBoard.flag_count(), cur_level.mineCount, $MainGame/GameBoard.flip_count(), cur_level.maxFlips)
+	if ret == $MainGame/GameBoard.OpResult_Win:
 		$GameUI.SetDialog(cur_level.win_dialog)
 	elif ret == $MainGame/GameBoard.OpResult_Lose:
 		$GameUI.SetDialog(cur_level.lose_dialog)
@@ -48,7 +40,7 @@ func _process(delta):
 	sum += delta
 	var mouse_pos: Vector2 = $MainGame.get_local_mouse_position()
 	
-	$MainGame/Cursor.position = Vector2(0, 10 * sin(sum)) + mouse_pos.clamp(Vector2(0,0) ,$MainGame.custom_minimum_size) - $MainGame/Cursor.size * $MainGame/Cursor.scale
+	$MainGame/Cursor.position = -$MainGame/Cursor.pivot_offset + Vector2(0, 10 * sin(sum)) + mouse_pos.clamp(Vector2(0,0) ,$MainGame.custom_minimum_size)
 	if !Rect2(Vector2(0, 0), $MainGame.custom_minimum_size).has_point(mouse_pos):
 		return
 	if !interactable:
